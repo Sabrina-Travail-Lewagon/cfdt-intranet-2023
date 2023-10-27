d9e341cd78ab9fa395fef9501cb82d0d66aad40b
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-10-27 10:48:01 +0200

    modif model article et category

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,13 @@
 class Article < ApplicationRecord
   belongs_to :user
+  # un article doit toujours être associé à un utilisateur lors de sa création ou de sa mise à jour:
+  validates :user, presence: true
+    # Ajout activeStorage aux articles pour pouvoir faciliter
+  # suppression images sur Cloudinary
+  # has_many_attached :images, service: :cloudinary, dependent: :purge
+  has_many_attached :images, dependent: :purge
+  # L'option dependent: :destroy garantit que si un article est supprimé,
+  # toutes les entrées correspondantes dans article_categories seront également supprimées
+  has_many :article_categories, dependent: :destroy
+  has_many :categories, through: :article_categories
 end
