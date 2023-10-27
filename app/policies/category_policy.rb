893d25aaad1c8a016b3749c1ebd742c8330b3fd3
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-10-27 09:39:07 +0200

    install pundit, et ajout policies Category et modif

---
+++ app/policies/category_policy.rb
@@ -0,0 +1,23 @@
+class CategoryPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+  def index?
+    user.admin? || user.rh? # Autorise admin ou rh à créer une catégorie
+  end
+  def show?
+    true
+  end
+  def create?
+    user.admin? || user.rh? # Autorise admin ou rh à créer une catégorie
+  end
+  def update?
+    user.admin? || user.rh? # Autorise admin ou rh à mettre à jour une catégorie
+  end
+  def destroy?
+    user.admin? || user.rh? # Autorise admin ou rh à supprimer une catégorie
+  end
+end
