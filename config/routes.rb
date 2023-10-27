16cf3177c72ae9bd13b15656c32621d9e60e6625
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-10-27 09:57:27 +0200

    ajout routes categories, et modif model category

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/edit'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'categories/new'
+  # get 'categories/edit'
   devise_for :users
   root to: "pages#home"
+  resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
