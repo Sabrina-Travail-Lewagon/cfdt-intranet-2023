57de96d1177c68c649b76ca8683c86ee8a8b4fbb
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-10-27 09:14:05 +0200

    ajout photo registration user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  ROLES = ['admin', 'user', 'rh', 'cse', 'redacteur'] # On stocke les rôles possibles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,17 +7,14 @@ class User < ApplicationRecord
   has_one_attached :photo
   validates :first_name, presence: true
   validates :last_name, presence: true
-
+  # validation inclusion, garantit que le role spécifié est une des valeurs autorisés
+  validates :role, inclusion: { in: ROLES }
   # before_validation définira automatiquement le rôle par défaut sur user avant de valider l'utilisateur.
   before_validation :set_default_role
 
-  # On stocke les rôles possibles
-  enum role: [:user, :cse, :rh, :redacteur, :admin]
-
   private
-
   # On va mettre le role user par défaut
   def set_default_role
-    self.role ||= :user
+    self.role ||= 'user'
   end
 end
