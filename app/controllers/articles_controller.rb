d28b76038aefe9459f5f5109648fa835e1bca8a5
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-10-27 10:27:45 +0200

    modif ctlr articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,35 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :destroy]
+  before_action :set_article, only: [:show, :edit, :destroy, :update]
   before_action :authenticate_user!
+  after_action :verify_authorized
 
   def index
     @user = current_user
-    @articles = Article.all.order('created_at DESC')
+    # Policy_scope  utilisée pour appliquer la politique de portée (policy scoping) aux collections d'enregistrements
+    # policy_scope(Article) applique la politique de portée à la collection d'articles,
+    # en fonction des autorisations définies dans votre politique ArticlePolicy
+    @articles = policy_scope(Article).order('created_at DESC')
+    authorize @articles
+  end
+
+  def new
+    @user = current_user
+    @article = Article.new
+    authorize @article
+  end
+
+  def create
+    @user = current_user
+    @article = Article.new(params_article)
+    @article.user = current_user # Pour éviter l'erreur l'user n'existe pas
+    authorize @article # Vérifie l'autorisation via Pundit
+    if @article.save
+      redirect_to articles_path(@article), notice: 'Article créé!'
+    else
+      console
+      flash.now[:alert] = "Impossible de créer l'article."
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -13,11 +38,28 @@ class ArticlesController < ApplicationController
   def edit
   end
 
+  def update
+    if @article.update(params_article)
+      redirect_to articles_path, :notice => "Article mis à jour."
+    else
+      redirect_to articles_path, :alert => "Impossible de mettre à jour l'article."
+    end
+  end
+
   def destroy
+    @article.destroy
+    redirect_to articles_path, notice: 'Article a bien été supprimé.'
   end
 
   private
+
   def set_article
     @article = Article.find(params[:id])
+    @user = current_user # Pour que le header puisse s'afficher avec le login
+    authorize @article
+  end
+
+  def params_article
+    params.require(:article).permit(:title, :rich_body, images: [], category_ids: [])
   end
 end
