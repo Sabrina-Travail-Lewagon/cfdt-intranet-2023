cf022835392951e2a3ef9389bb8448fafa8371ef
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-10-27 09:35:43 +0200

    ajout images par défaut, ajout de la partie categories BdD et controller categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,28 @@
 class User < ApplicationRecord
-  ROLES = ['admin', 'user', 'rh', 'cse', 'redacteur'] # On stocke les rôles possibles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one_attached :photo
   validates :first_name, presence: true
   validates :last_name, presence: true
   # validation inclusion, garantit que le role spécifié est une des valeurs autorisés
-  validates :role, inclusion: { in: ROLES }
+  # validates :role, inclusion: { in: ROLES }
+
   # before_validation définira automatiquement le rôle par défaut sur user avant de valider l'utilisateur.
   before_validation :set_default_role
 
+  # On stocke les rôles possibles
+  enum role: [:user, :cse, :rh, :redacteur, :admin]
+  # after_initialize :set_default_role, if: :new_record?
+  # Ajout photo a l'utilisateur
+  has_one_attached :photo
+
   private
   # On va mettre le role user par défaut
+  # def set_default_role
+  #   self.role ||= 'user'
+  # end
   def set_default_role
-    self.role ||= 'user'
+    self.role ||= :user
   end
 end
