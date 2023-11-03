cd92e0f50036cd3519e4c0681c3b9c9c8d0d69e1
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-11-03 15:17:48 +0100

    Add to Scalingo

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,11 +4,12 @@ class Article < ApplicationRecord
 
   # un article doit toujours être associé à un utilisateur lors de sa création ou de sa mise à jour:
   validates :user, presence: true
-    # Ajout activeStorage aux articles pour pouvoir faciliter
+  # Ajout activeStorage aux articles pour pouvoir faciliter
   # suppression images sur Cloudinary
   # has_many_attached :images, service: :cloudinary, dependent: :purge
   has_many_attached :images, dependent: :purge
   # L'option dependent: :destroy garantit que si un article est supprimé,
   # toutes les entrées correspondantes dans article_categories seront également supprimées
   has_rich_text :rich_body
+  #
 end
