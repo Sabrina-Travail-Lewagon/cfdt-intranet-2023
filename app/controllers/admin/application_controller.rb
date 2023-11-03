31756c3235463fca9a43d31c97fc9dd4bd244b9f
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-11-03 10:15:56 +0100

    ajout breadcrumb dans admin/application_controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,6 +2,7 @@ class Admin::ApplicationController < ApplicationController
   before_action :authenticate_user!
   include Pundit::Authorization
   before_action :authorize_admin!
+  before_action :set_dashboard_breadcrumbs
 
   private
   def authorize_admin!
@@ -9,6 +10,12 @@ class Admin::ApplicationController < ApplicationController
     unless current_user.admin? || current_user.rh? || current_user.cse? || current_user.user?
       redirect_to root_path, alert: "Vous n'avez pas accès à cette section."
     end
+  end
+
+  def set_dashboard_breadcrumbs
+    @breadcrumbs = [
+      { name: 'Dashboard', path: admin_root_path }
+    ]
   end
 
 end
