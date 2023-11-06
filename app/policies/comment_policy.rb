f9426386e8279a40c8cb6e7963b6b62ced75578c
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-11-06 15:02:48 +0100

    ajout des policies dans comment_policy.rb

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -5,4 +5,16 @@ class CommentPolicy < ApplicationPolicy
     #   scope.all
     # end
   end
+
+  def create?
+    user.present?
+  end
+
+  def update?
+    user.present? && (record.user == user || user.admin?)
+  end
+
+  def destroy?
+    user.present? && (record.user == user || user.admin?)
+  end
 end
