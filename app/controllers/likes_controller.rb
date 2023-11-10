9bb6e210510276d386ff9f9834e8d2761ec94a99
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-11-10 15:43:39 +0100

    ajout methodes dans le controller like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,24 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    @article = Article.find(params[:article_id])
+    @rating = @article.ratings.new(rating_params)
+    @rating.user = current_user
+
+    if @rating.save
+      redirect_to @article, notice: 'J\'aime créé avec succés.'
+    else
+      redirect_to @article, alert: 'Il y a eu une erreur.'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def rating_params
+    params.require(:rating).permit(:score)
   end
 end
