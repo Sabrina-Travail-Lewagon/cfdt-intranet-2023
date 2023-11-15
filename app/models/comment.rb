a90a3476cf50fe3c85695e86115257439def1142
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-11-15 16:18:00 +0100

    modfi model comment, et ajout custom msg dans locales/fr.yml perso error messages

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :article
   # Valide que le contenu est présent
-  validates :content, presence: { message: 'Le commentaire ne peut pas être vide.' }
+  validates :content, presence: { message: :empty }
 
   # Valide que le contenu a une longueur minimale de 10 caractères
-  validates :content, length: { minimum: 10, message: 'Le commentaire doit contenir au moins 10 caractères.' }
+  validates :content, length: { minimum: 10, message: :too_short }
+  # :empty et :too_short sont des symboles qui correspondent à des clés de localisation.
 end
