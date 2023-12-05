69caf7ae9c0d815de4b3176def66ec6664b54c47
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-12-05 11:24:33 +0100

    config

---
+++ app/javascript/ckeditor/config.js
@@ -0,0 +1,5 @@
+CKEDITOR.editorConfig = function(config) {
+  // Définissez vos configurations personnalisées ici
+  config.language = 'fr'; // Pour définir la langue en français
+  // Autres configurations...
+};
