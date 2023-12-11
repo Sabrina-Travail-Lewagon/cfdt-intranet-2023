a64208e6879c99334448553b65169162f88757a1
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-12-11 11:18:02 +0100

    ajout validation sur l'image de la catégorie

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
   validates :nom, presence: true
+  validates :image, presence: true
+
   has_many :articles
   has_one_attached :image
   # Ajout de l'attribut pour la couleur de fond
