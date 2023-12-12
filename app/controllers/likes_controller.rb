26a73763e8b424a47a7e12b2da021f3880f91434
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-12-12 16:54:30 +0100

    mise en français des notifs likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,7 +6,7 @@ class LikesController < ApplicationController
     @like = @article.likes.new(user: current_user)
     authorize @like
     if @like.save
-      flash[:notice] = 'Vous avez liké cet article.'
+      flash[:notice] = 'Vous avez aimé cet article.'
     else
       # Le message ici devrait refléter l'échec de la création du like, pas le "dé-like"
       flash[:alert] = 'Un problème est survenu, vous ne pouvez pas liker cet article.'
@@ -18,10 +18,10 @@ class LikesController < ApplicationController
     @like = @article.likes.find_by(user_id: current_user.id)
     authorize @like
     if @like.destroy
-      flash[:notice] = 'Vous avez retiré votre like de cet article.'
+      flash[:notice] = 'Vous avez retiré votre j\'aime de cet article.'
     else
       # Ce message s'affiche si l'utilisateur essaie de retirer un like qui n'existe pas ou s'il y a une erreur de suppression
-      flash[:alert] = 'Vous ne pouvez pas retirer un like d\'un article que vous n\'avez pas liké ou une erreur est survenue.'
+      flash[:alert] = 'Vous ne pouvez pas retirer un j\'aime d\'un article que vous n\'avez pas aimé ou une erreur est survenue.'
     end
     redirect_to articles_path
   end
