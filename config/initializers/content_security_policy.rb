441e637ea15bad1f5280dd8bcf40aa19051e89dc
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-12-13 10:29:29 +0100

    modif CSP, pour plus secure, ajout css inline pour btn dans mail

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -32,11 +32,11 @@ Rails.application.config.content_security_policy do |policy|
   # Allow images from 'self' and Cloudinary
   policy.img_src :self, :data, 'https://res.cloudinary.com/dooup7bi2/image/upload/', 'blob:'
   # Allow blob URLs for PDFs
-  policy.object_src :self, 'blob:'
+  policy.object_src :none, 'blob:'
 
   # Scripts and styles for the text editor
   # You might need to add specific hashes or nonces if your text editor uses inline scripts or styles
-  policy.script_src :self, :unsafe_inline
+  policy.script_src :self
 
   # Update style-src to include Google Fonts
   policy.style_src :self, :unsafe_inline, 'https://fonts.googleapis.com'
@@ -47,8 +47,8 @@ Rails.application.config.content_security_policy do |policy|
 
   # Uncomment report_uri if you want to ensure that violations are reported
   # policy.report_uri "/csp-violation-report-endpoint"
-  policy.report_uri "/csp_violation_report"
+  # policy.report_uri "/csp_violation_report"
 end
 
 # Uncomment if you want to use Content Security Policy in report-only mode for testing
-Rails.application.config.content_security_policy_report_only = true
+# Rails.application.config.content_security_policy_report_only = true
