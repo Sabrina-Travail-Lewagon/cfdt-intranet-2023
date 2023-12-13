416b4e65cc8246c17f34d755f1522b1fde0d8a65
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-12-13 10:46:11 +0100

    modif dashboard-bar pour hauteur ecran, et modif acces mobile user

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Admin::RegistrationsController < Devise::RegistrationsController
+  before_action :restrict_access_on_mobile
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]  # Assurez-vous que :user correspond à votre modèle Devise
   end
@@ -8,5 +9,10 @@ class Admin::RegistrationsController < Devise::RegistrationsController
 
   def update
     super
+  end
+  private
+  # Restriction de l'accès sur mobile
+  def restrict_access_on_mobile
+    redirect_to root_path, alert: "L'accès admin n'est pas disponible sur les téléphones" if device_type == :mobile
   end
 end
