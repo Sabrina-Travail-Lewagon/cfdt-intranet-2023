60ea197ac99899f65a3acb0d22df6d5cfbc2f1b3
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-12-14 13:42:38 +0100

    modif ctrl errors ajout variable num error, et affichage dans title layout errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,12 @@
 class ErrorsController < ActionController::Base
   def internal_server_error
+    # ON récupère le numéro de l'erreur pour le title
+    @error_code = 500
     render status: 500
   end
 
   def not_found
+    @error_code = 404
     render status: 404
   end
 end
